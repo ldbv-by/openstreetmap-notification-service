0a7397ed1ddeb2c8446b540689cb9a507107344e
Tobias Walter <tob-walter@gmx.de>
2026-02-05 07:38:49 +0100

    Bugfix insert statement

--- src/main/resources/sql/notification_types.sql
+++ src/main/resources/sql/notification_types.sql
@@ -1,4 +1,4 @@
 INSERT INTO notification_data.notification_types (id, description) VALUES
     ('INFO', 'Information'),
     ('QUALITY_CHECK', 'Qualitätsprüfung')
-ON CONFLICT (object_type) DO NOTHING;
+ON CONFLICT (id) DO NOTHING;
